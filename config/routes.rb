39e9d715b654c0384fc13c72c9865326d3fed5ba
vikasn71 <vikasn71@gmail.com>
2016-01-25 16:32:04 -0500

    modified

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'departments#index'
    get 'orderbyage', to: 'employees#orderbyage'
    get 'orderbydepartment', to: 'employees#orderbydepartment'
    get 'orderbysalary', to: 'employees#orderbysalary'
